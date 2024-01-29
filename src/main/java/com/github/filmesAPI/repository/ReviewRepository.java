32563a0f832ac3ac4aebeec8f6fd64a1ccf4a3c6
Vitor <vitornuneschagas2016@gmail.com>
2024-01-29 19:27:14 -0300

    feat: Repositorio das avaliações

---
+++ src/main/java/com/github/filmesAPI/repository/ReviewRepository.java
@@ -0,0 +1,12 @@
+package com.github.filmesAPI.repository;
+
+import com.github.filmesAPI.entities.models.ReviewModel;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<ReviewModel, Long> {
+}
