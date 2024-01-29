8535d8b50ce12d6d4c6006d71a20f66b9cab0dbe
Vitor <vitornuneschagas2016@gmail.com>
2024-01-29 14:29:11 -0300

    inital commit

---
+++ src/main/java/com/github/filmesAPI/FilmesApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.filmesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmesApiApplication.class, args);
+	}
+
+}
